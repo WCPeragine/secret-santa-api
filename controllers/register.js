780a316afb0de0078a581100dd8bda60b8b16b34
wcperagine <ertyjunior@gmail.com>
2018-08-31 05:55:17 +0000

    no register return

--- controllers/register.js
+++ controllers/register.js
@@ -9,14 +9,14 @@ function handleRegister(req, res, db, bcrypt) {
 	.update({
 		hash: hash
 	})
-	.returning('email')
-	.then(loginEmail => {
-		return db.select('*').from('users')
-		.where('email', '=', loginEmail)
-		.then(user => {
-			res.json(user[0])
-		})
-		.catch(err => res.status(400).json('Can not find user'))
-	})
+	// .returning('email')
+	// .then(loginEmail => {
+	// 	return db.select('*').from('users')
+	// 	.where('email', '=', loginEmail)
+	// 	.then(user => {
+	// 		res.json(user[0])
+	// 	})
+	// 	.catch(err => res.status(400).json('Can not find user'))
+	// })
 	.catch(err => res.status(400).json('Incorrect Email'))
 }
