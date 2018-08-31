eef9726a13010a396a24b56d5a446170d3d342d2
wcperagine <ertyjunior@gmail.com>
2018-08-31 06:33:34 +0000

    return register

--- controllers/register.js
+++ controllers/register.js
@@ -9,16 +9,24 @@ function handleRegister(req, res, db, bcrypt){
 	.update({
 		hash: hash
 	})
-	.returning('email')
-	.then(loginEmail => {
-		return db.select('*').from('users')
-		.where('email', '=', loginEmail)
-		.then(user => {
-			res.json(user[0])
-		})
-		.catch(err => res.status(400).json('Can not find user'))
-	})
 	.catch(err => res.status(400).json('Incorrect Email'))
+
+	db.select('email', 'hash').from('login')
+	.where('email', '=', email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(password, data[0].hash);
+		if (isValid) {
+			return db.select('*').from('users')
+			.where('email', '=', email)
+			.then(user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('Could not find user'))
+		} else {
+			res.status(400).json('Incorrect Credentials')
+		}
+	})
+	.catch(err => res.status(400).json('Incorrect Credentials'))
 }
 
 module.exports = {
