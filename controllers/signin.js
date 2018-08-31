34047c3dbebda28863fd87eec8edcb958e28a87a
wcperagine <ertyjunior@gmail.com>
2018-08-31 06:11:45 +0000

    data[0]

--- controllers/signin.js
+++ controllers/signin.js
@@ -6,7 +6,7 @@ function handleSignIn(req, res, db, bcrypt){
 	db.select('email', 'hash').from('login')
 	.where('email', '=', email)
 	.then(data => {
-		const isValid = bcrypt.compareSync(password, data[1].hash);
+		const isValid = bcrypt.compareSync(password, data[0].hash);
 		if (isValid) {
 			return db.select('*').from('users')
 			.where('email', '=', email)
