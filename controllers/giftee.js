24c81adf3312ebc9bb5f1824a451ccd1262dbc59
wcperagine <ertyjunior@gmail.com>
2018-09-01 06:32:10 +0000

    fixed again

--- controllers/giftee.js
+++ controllers/giftee.js
@@ -1,7 +1,12 @@
 function selectGiftee(req, res, db){
 	db.select('name', 'user_id', 'giftee_id', 'group_id').from('users')
 	.then (data => {
-		res.json(data[0])
+		if (data.length) {
+			res.json(data)
+		} else {
+			res.status(400).json('Unable to find giftees')
+		}
+
 	})
 	.catch(err => res.status(400).json('Could not load giftees'))
 }
