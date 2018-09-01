bbc9cfaae25a1baf397302503d244f7685423930
wcperagine <ertyjunior@gmail.com>
2018-09-01 07:15:50 +0000

    res.json data

--- controllers/giftee.js
+++ controllers/giftee.js
@@ -16,10 +16,11 @@ function setGiftee(req, res, db){
 	if (!user_id || !giftee_id || user_id === giftee_id) {
 		return res.status(400).json('Incorrect information')
 	}
-	return res.json(user_id)
+
 	db.select('giftee_id').from('users')
 	.where('user_id', '=', user_id)
 	.then(data => {
+		return res.json(data)
 		if(data.length){
 			db('users').where('user_id', '=', user_id)
 			.update({
