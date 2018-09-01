5db32c303bca8d0ce7d23c1fa9ee986e25ca1326
wcperagine <ertyjunior@gmail.com>
2018-09-01 06:25:52 +0000

    err

--- controllers/giftee.js
+++ controllers/giftee.js
@@ -1,11 +1,12 @@
 function selectGiftee(req, res, db){
 	db.select('name', 'user_id', 'giftee_id', 'group_id').from('users')
 	.then (data => {
-		if(data.length) {
-			res,json(data[0])
-		} else {
-			res.status(400).json('not found')
-		}
+		console.log(data)
+		// if(data.length) {
+		// 	res,json(data[0])
+		// } else {
+		// 	res.status(400).json('not found')
+		// }
 	})
 	.catch(err => res.status(400).json('Could not load giftees'))
 }
