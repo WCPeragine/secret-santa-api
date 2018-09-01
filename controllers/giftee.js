60e640ccc95d41abecc82db32c6d8c60ba4f638c
wcperagine <ertyjunior@gmail.com>
2018-09-01 06:29:15 +0000

    fixed

--- controllers/giftee.js
+++ controllers/giftee.js
@@ -1,12 +1,7 @@
 function selectGiftee(req, res, db){
 	db.select('name', 'user_id', 'giftee_id', 'group_id').from('users')
 	.then (data => {
-		console.log(data)
-		// if(data.length) {
-		// 	res,json(data[0])
-		// } else {
-		// 	res.status(400).json('not found')
-		// }
+		res.json(data[0])
 	})
 	.catch(err => res.status(400).json('Could not load giftees'))
 }
