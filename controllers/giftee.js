79218dcd5eb0517338e278a5c208922efb1fd6c1
wcperagine <ertyjunior@gmail.com>
2018-09-01 07:39:17 +0000

    testing

--- controllers/giftee.js
+++ controllers/giftee.js
@@ -17,8 +17,9 @@ function setGiftee(req, res, db){
 		return res.status(400).json('Incorrect information')
 	}
 
-	db.select('giftee_id').from('users')
+	db.select('giftee_id', 'group_id').from('users')
 	.where('user_id', '=', user_id)
+	.orWhere('user_id', '=', giftee_id)
 	.then(data => {
 		if(!data[0].giftee_id){
 			db('users').where('user_id', '=', user_id)
@@ -28,7 +29,7 @@ function setGiftee(req, res, db){
 			.then(resp => {
 				res.json("Successfully registered giftee")
 			})
-			.catch(err => res.status(400).json('Giftee taken already'))
+			.catch(err => res.status(400).json('Giftee already taken'))
 		} else {
 			res.status(400).json('User already has a giftee')
 		}
