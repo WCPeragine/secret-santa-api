854eb4339fca6d98763d1e5a49afe885972fc986
wcperagine <ertyjunior@gmail.com>
2018-09-08 07:47:45 +0000

    fixing user return

--- controllers/register.js
+++ controllers/register.js
@@ -18,22 +18,12 @@ function handleRegister(req, res, db, bcrypt){
 			})
 			.catch(err => res.status(400).json('Incorrect Email'))
 
-			db.select('email', 'hash').from('login')
+			return db.select('*').from('users')
 			.where('email', '=', email)
-			.then(data => {
-				const isValid = bcrypt.compareSync(password, data[0].hash);
-				if (isValid) {
-					return db.select('*').from('users')
-					.where('email', '=', email)
-					.then(user => {
-						res.json(user[0])
-					})
-					.catch(err => res.status(400).json('Could not find user'))
-				} else {
-					res.status(400).json('Incorrect Credentials')
-				}
+			.then(user => {
+				res.json(user[0])
 			})
-			.catch(err => res.status(400).json('Incorrect Credentials'))
+			.catch(err => res.status(400).json('Could not find user'))
 		}
 	})
 
